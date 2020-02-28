b0eac4160a1ad0e6e8e9699b2b4235cb4c525ee7
EC2 Default User <ec2-user@ip-172-31-43-127.us-east-2.compute.internal>
2020-02-28 17:28:07 +0000

    added new paper

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,13 +1,20 @@
 class PapersController < ApplicationController
   
+  def paper_params
+    params.require(:paper).permit(:title)
+  end
+  
   def index
     @papers = Paper.order(:title)
   end
   
-  def new
-    @paper = Paper.create!()
+  def create
+    @paper = Paper.create!(paper_params)
     flash[:notice] = "#{@paper.title} was successfully created."
     redirect_to papers_path
+  end
+  
+  def new
   end
   
 end
