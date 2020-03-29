539accccbc61024108d445b239d6ef76a78a5b00
Ubuntu <ubuntu@ip-172-31-31-110.us-east-2.compute.internal>
2020-03-29 01:14:57 +0000

    comp

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_03_28_163009) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "email"
