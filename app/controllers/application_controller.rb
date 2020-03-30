eca8e3dd450a04084f6b5bdd1e33a6c65bc15224
Ubuntu <ubuntu@ip-172-31-40-238.us-east-2.compute.internal>
2020-03-30 12:59:51 +0000

    login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || professor_path
+    end
+    def after_sign_un_path_for(resource)
+      stored_location_for(resource) || professor_path
+    end
 end
